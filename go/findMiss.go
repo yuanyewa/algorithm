21c9e1c7a8872875720a82a1b3dd569951f6c6fb
Fake Person <afakeid@yahoo.com>
2018-12-28 21:42:20 -0800

    add func

--- go/findMiss.go
+++ go/findMiss.go
@@ -34,6 +34,28 @@ func miss2(x, y []int) int {
 	}
 	return x[len(x)-1]
 }
+
+func miss3(x, y []int) int {
+	d := make(map[int]int)
+	for _, v := range y {
+		d[v]++
+	}
+	for _, v := range x {
+		if d[v] == 0 {
+			return v
+		}
+		d[v]--
+	}
+	return -1
+}
+
+func miss4(x, y []int) int {
+	r := 0
+	for _, v := range append(x, y...) {
+		r ^= v
+	}
+	return r
+}
 func main() {
 	start := time.Now()
 	for i := 0; i < 1000000; i++ {
@@ -43,6 +65,17 @@ func main() {
 	start = time.Now()
 	for i := 0; i < 1000000; i++ {
 		miss2([]int{1, 2, 3, 4, 5, 6, 7}, []int{3, 7, 2, 1, 4, 6})
+	}
+	fmt.Println(time.Since(start))
+	start = time.Now()
+	for i := 0; i < 1000000; i++ {
+		miss3([]int{1, 2, 3, 4, 5, 6, 7}, []int{3, 7, 2, 1, 4, 6})
+	}
+	fmt.Println(time.Since(start))
+	fmt.Println(miss4([]int{1, 2, 3, 4, 5, 6, 7}, []int{3, 5, 2, 1, 4, 6}))
+	start = time.Now()
+	for i := 0; i < 1000000; i++ {
+		miss4([]int{1, 2, 3, 4, 5, 6, 7}, []int{3, 7, 2, 1, 4, 6})
 	}
 	fmt.Println(time.Since(start))
 }
